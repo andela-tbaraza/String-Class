c0fc9277280422fb2691ca6e7fd201ddd652bbad
Tonida Baraza <tonida.baraza@andela.com>
2016-07-05 23:20:32 +0300

    fix the spec folder name

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function(config) {
     // list of files / patterns to load in the browser
     files: [
       'src/string-class.js',
-      '/jasmine/spec/stringClassSpec.js'
+      '/jasmine/spec/StringClassSpec.js'
     ],
 
 
