0fdbffe694fdee17a3d80040ab9e84dc8171a567
Tonida Baraza <tonida.baraza@andela.com>
2016-07-06 00:03:08 +0300

    configure karma for overage

--- karma.conf.js
+++ karma.conf.js
@@ -28,13 +28,14 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'src/string-class.js': ['coverage']
     },
 
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['progress', 'coverage'],
 
 
     // web server port
@@ -56,7 +57,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Firefox'],
+    browsers: ['phantomJS'],
 
 
     // Continuous Integration mode
