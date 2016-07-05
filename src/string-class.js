522450784e1081b167ea0d9189e81d4d5fabf2d5
Tonida Baraza <tonida.baraza@andela.com>
2016-07-05 13:36:51 +0300

    added comments

--- src/string-class.js
+++ src/string-class.js
@@ -1,9 +1,13 @@
 String.prototype.hasVowels = function() {
+  /* this method returns true if there is a vowels
+     in a string and false for absence of vowels
+  */
   const vowels = new RegExp('[aeiou]', 'i');
   return vowels.test(this) ? true : false;
 };
 
 String.prototype.toUpper = function () {
+  /* converts a string to uppercase format */
   let newString = '';
   const upper = new RegExp('[a-z]');
   for (let index = 0; index < this.length; index++) {
@@ -15,6 +19,7 @@ String.prototype.toUpper = function () {
 };
 
 String.prototype.toLower = function () {
+  /* converts a string to lowercase format */
   let newString = '';
   const upper = new RegExp('[A-Z]');
   for (let index = 0; index < this.length; index++) {
@@ -26,20 +31,24 @@ String.prototype.toLower = function () {
 };
 
 String.prototype.ucFirst = function () {
+  /* converts the forst letter of a string to uppercase */
   return [this.charAt(0).toUpper(), this.slice(1)].join('');
 };
 
 String.prototype.isQuestion = function () {
+  /* checks if a string is a question */
   const question = new RegExp(/\?$/);
   return question.test(this) ? true : false;
 };
 
 String.prototype.words = function () {
+  /* makes an array out of a string */
   const boundary = new RegExp(/\W+/);
   return this.split(boundary);
 };
 
 String.prototype.wordCount = function () {
+  /* gives the count of words in a string */
   let count = 0;
   const newArray = this.words();
   newArray.forEach(() => {
@@ -49,11 +58,13 @@ String.prototype.wordCount = function () {
 };
 
 String.prototype.toCurrency = function () {
+  /* returns a currency format of a string */
   let floatString = parseFloat(this);
   let currencyString = floatString.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, '$1,');
   return currencyString;
 };
 
 String.prototype.fromCurrency = function () {
+  /* returns the number format of a string */
   return Number(this.replace(/\,/g, ''));
 };
